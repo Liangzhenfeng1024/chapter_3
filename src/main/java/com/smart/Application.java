1a76c8f5c27d4c8e8adae179a838180bdf7ea121
liangzhenfeng <925982329@qq.com>
2017-12-25 19:33:28 +0800

    first commit

---
+++ src/main/java/com/smart/Application.java
@@ -0,0 +1,21 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+@SpringApplicationConfiguration
+@EnableAutoConfiguration //启用注解事务管理
+public class Application extends SpringBootServletInitializer{
+
+    public static void mian(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
